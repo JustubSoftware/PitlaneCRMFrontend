493b85ed0c0d4b9a2327932535b66c2365e15aa5
Justin Becker <justin.becker1@gmx.de>
2025-04-15 12:31:58 +0200

    more style fix

--- pitlanefrontend/src/routes/customers/+page.svelte
+++ pitlanefrontend/src/routes/customers/+page.svelte
@@ -180,7 +180,7 @@
                 <td class="py-4 px-4 whitespace-nowrap">
                   <span
                     on:click={() => openEditModal(customer)}
-                    class="material-icons text-green-500 hover:text-blue-600 cursor-pointer align-middle"
+                    class="material-icons text-green-500 hover:text-green-800 cursor-pointer align-middle"
                     title="Bearbeiten"
                     style="font-size: 20px; vertical-align: middle;"
                   >
@@ -188,7 +188,7 @@
                   </span>
                   <span
                     on:click={() => openDeleteConfirmation(customer)}
-                    class="material-icons text-red-500 hover:text-red-600 cursor-pointer align-middle"
+                    class="material-icons text-red-500 hover:text-red-800 cursor-pointer align-middle"
                     title="Löschen"
                     style="font-size: 20px; vertical-align: middle;"
                   >
